ba5dfcb3ad5b579349fdd6993cc54eb1bfb90c3a
shaimaa <shaimafikry@yahoo.com>
2024-03-07 14:37:44 +0200

    edit file 12

--- functions_monty.c
+++ functions_monty.c
@@ -59,9 +59,9 @@ while (current != NULL)
 */
 void pint_monty(stack_t **stack, unsigned int line_number)
 {
-if (stack == NULL)
+if (*stack == NULL)
 {
-	fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 	exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
